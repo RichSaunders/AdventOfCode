588e6254ec642f98fc6bb04bbfa00b0c5e340971
Rich Saunders <cosmicchopsticks@gmail.com>
2020-12-10 11:17:39 +0000

    Refactored d10p2 to only do one pass through the list

--- 2020/day10/part2.py
+++ 2020/day10/part2.py
@@ -2,28 +2,18 @@ with open("input.txt", "r") as f:
     input = f.read()
 adapters = [int(x) for x in input.splitlines()]
 adapters.sort()
+# Add the start and end points, which are 0 and the largest value + 3
 adapters = [0] + adapters + [adapters[-1] + 3]
 
-# Build out a list of possible next adapters for each one
-combinations = []
-for i in range(len(adapters) - 1):
-    indexes = []
+# Going backwards, check which adapters can follow each one, and add up the number of paths forwards from there
+permutations = [0] * (len(adapters)-1) + [1]
+for i in reversed(range(len(adapters)-1)):
+    permutations[i] += permutations[i+1]
 
-    if i + 1 < len(adapters) and adapters[i + 1] - adapters[i] < 4:
-        indexes.append(i + 1)
+    if i + 2 < len(adapters) and adapters[i+2] - adapters[i] <= 3:
+        permutations[i] += permutations[i+2]
 
-        if i + 2 < len(adapters) and adapters[i + 2] - adapters[i] < 4:
-            indexes.append(i + 2)
-
-            if i + 3 < len(adapters) and adapters[i + 3] - adapters[i] < 4:
-                indexes.append(i + 3)
-
-    combinations.append(indexes)
-
-# Going backwards, build out list of how many possibilities there are from that point
-# We know which adapters can follow which, so just add up the possibilities
-permutations = [1] * len(adapters)
-for i in reversed(range(len(combinations))):
-    permutations[i] = sum([permutations[x] for x in combinations[i]])
+        if i + 3 < len(adapters) and adapters[i+3] - adapters[i] <= 3:
+            permutations[i] += permutations[i+3]
 
 print(permutations[0])
