d3c67cb2d058b7f9f1a82aa2dacab1bba95bac9d
Rich Saunders <cosmicchopsticks@gmail.com>
2021-12-11 17:38:28 +0000

    Fix mistake in Day 11. Misread the question, somehow still got the right answer

--- 2021/day11/part2.py
+++ 2021/day11/part2.py
@@ -9,10 +9,12 @@ dX = [-1, 0, 1, -1, 1, -1, 0, 1]
 dY = [-1, -1, -1, 0, 0, 1, 1, 1]
 
 for j in range(1000):
+    octopi += 1
+
     charged = []
     for x in range(octopi.shape[0]):
         for y in range(octopi.shape[1]):
-            if octopi[x,y] >= 9:
+            if octopi[x,y] == 10:
                 charged.append((x,y))
 
     for octopus in charged:
@@ -21,14 +23,12 @@ for j in range(1000):
             y = octopus[1]+dY[i]
             if 0 <= x < octopi.shape[0] and 0 <= y < octopi.shape[1]:
                 octopi[x, y] += 1
-                if octopi[x,y] == 9:
+                if octopi[x,y] == 10:
                     charged.append((x,y))
 
     if len(charged) == octopi.shape[0] * octopi.shape[1]:
         print(j+1)
         break
-
-    octopi += 1
 
     for octopus in charged:
         octopi[octopus] = 0
