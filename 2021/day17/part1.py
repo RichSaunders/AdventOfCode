98547bc92e2542941d416b08182941f76404f88b
Rich Saunders <cosmicchopsticks@gmail.com>
2021-12-17 12:27:55 +0000

    Made y_range consistent with part 2

--- 2021/day17/part1.py
+++ 2021/day17/part1.py
@@ -2,7 +2,7 @@ import re
 
 with open("input.txt") as f:
     match = re.match("target area: x=([\d-]+)..([\d-]+), y=([\d-]+)..([\d-]+)", f.read())
-    y_range = [int(match.group(3)), int(match.group(4))]
+    y_range = [int(match.group(4)), int(match.group(3))]
 
-y_velocity = y_range[0]*-1 - 1
+y_velocity = y_range[1]*-1 - 1
 print(0.5*(y_velocity**2 + y_velocity))
