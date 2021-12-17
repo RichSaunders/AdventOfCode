6159996ce7d0169e3f467e669787c27ad2dedaec
Rich Saunders <cosmicchopsticks@gmail.com>
2021-12-17 12:25:57 +0000

    Cleaned part 1

--- 2021/day17/part1.py
+++ 2021/day17/part1.py
@@ -4,8 +4,5 @@ with open("input.txt") as f:
     match = re.match("target area: x=([\d-]+)..([\d-]+), y=([\d-]+)..([\d-]+)", f.read())
     y_range = [int(match.group(3)), int(match.group(4))]
 
-def triangle(x):
-    return 0.5*(x**2 + x)
-
 y_velocity = y_range[0]*-1 - 1
-print(triangle(y_velocity))
+print(0.5*(y_velocity**2 + y_velocity))
